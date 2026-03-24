dfcaf0f43bb4ce2649f4e66c37b36f9696bddc55
Eduardo Fraga <fragaeduardo545@gmail.com>
2026-03-24 20:31:17 -0300

    Finalizando o estilo da página

---
+++ script.js
@@ -0,0 +1,7 @@
+//SIMULAÇÃO DE ADIÇÃO DE ITENS AO PEDIDO
+//SELECIONA TODOS OS BOTÕES QUE POSSUEM A CLASSE 'ADICIONAR'
+
+const botoesAdicionar = document.querySelectorAll(".adicionar");
+
+//SELECIONA A LISTA ONDE OS ITENS DO PEDIDO SERÃO EXIBIDOS
+const listaPedido = document.getElementById("lista-pedido");
