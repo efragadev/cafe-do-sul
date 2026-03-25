c28e31840815e7dcdc3e0503959ae570e48724d1
Eduardo Fraga <fragaeduardo545@gmail.com>
2026-03-24 21:15:33 -0300

    Iniciando o javascript da página

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const botoesAdicionar = document.querySelectorAll(".adicionar");
 
 //SELECIONA A LISTA ONDE OS ITENS DO PEDIDO SERÃO EXIBIDOS
 const listaPedido = document.getElementById("lista-pedido");
+
+//SELECIONA O ELEMENTO QUE EXIBIRÁ O VALOR TOTAL DO PEDIDO
+const totalElemento = document.querySelectorById("total");
+
+
+//VARIAVEL PARA ARMAZENAR O TOTAL DO PEDIDO
+let total = 0;
+
+//PERCORRE TODOS OS BOTÕES "ADICIONAR" E ADICONA UM EVENTO DE CLIQUE A CADA UM DELES
+botoesAdicionar.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        
+    })
+});
